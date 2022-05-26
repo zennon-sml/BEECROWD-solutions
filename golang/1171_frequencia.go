004d506d4053c428546e7ef3e718e916c397d31b
zennon-sml <zennonsampaioml@gmail.com>
2022-05-26 04:53:52 -0300

    iiiiiiiiiirrrru consegui porra, ac 1171

--- golang/1171_frequencia.go
+++ golang/1171_frequencia.go
@@ -13,22 +13,22 @@ func in(slice []int, n int) (int, bool) {
     return 0, true
 }
 
-func deleteMin(slice []int) (int, int){
+func min(slice []int) (int, int){
     menor := slice[0]
     var index1 int
     for index, item := range slice{
+        if item < 2001{
         if item < menor{
             menor = item
             index1 = index
-            slice[index]
-        }
+        }}
     }
     return menor, index1
 }
 
 func main(){
     var n,n1 int
-    var slice, slice1 []int
+    var slice, slice1, slice2, slice3 []int
     fmt.Scanf("%d", &n)
     for n > 0{
         n--
@@ -41,8 +41,14 @@ func main(){
             slice1[index]++
         }
     }//endfor
-
-    for index, item := range slice{
-        fmt.Printf("%d aparece %d vez(es)\n", item, slice1[index])
+    tam := len(slice)
+    for i := 0;i < tam;i++{
+        menor, mindex := min(slice)
+        slice2 = append(slice2, menor)
+        slice3 = append(slice3, slice1[mindex])
+        slice[mindex] = 2001
+    }
+    for index, item := range slice2{
+        fmt.Printf("%d aparece %d vez(es)\n", item, slice3[index])
     }
 }
