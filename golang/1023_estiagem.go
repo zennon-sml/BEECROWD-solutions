29fd0e53992bf3ff0737cd3160f09f287ae2f6df
zennon-sml <zennonsampaioml@gmail.com>
2022-05-31 04:13:14 -0300

    undone 1023 estiagem timelimitex

--- golang/1023_estiagem.go
+++ golang/1023_estiagem.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+    "sort"
+    "math"
 )
 
 type Consumo struct {
@@ -9,28 +11,53 @@ type Consumo struct {
 	tagua float64
 }
 
+func Round(x, uint float64) float64{
+	return math.Round(x/uint) * uint
+}
+
 func main() {
 	var n int
+    count := 0
 	for true {
+        count++
 		fmt.Scanf("%d", &n)
 		if n == 0 {
 			break
 		}
+		if count > 1{
+			fmt.Println()
+		}
 		var pess, agua float64
 		var total []Consumo
-		var totalKeys []string
 		tPess := 0.0
 		tAgua := 0.0
 		for i := 0; i < n; i++ {
 			fmt.Scanf("%f%f", &pess, &agua)
 			tPess += pess
 			tAgua += agua
-			tConsumo := Consumo{tpess: pess, tagua: agua / pess}
+			tConsumo := Consumo{tpess: pess, tagua: math.Trunc(agua / pess)}
 			total = append(total, tConsumo)
 		}
-		chaves := make([]string, 0, len(total))
-		fmt.Println(total)
-	}
+		for i := 0; i < n;i++{
+			for j := i+1; j < n;j++{
+				if total[i].tagua == total[j].tagua{
+					total[i].tpess += total[j].tpess
+					total[j].tpess = 0
+					total[j].tagua = 0
+				}
+			}
+		}
+        sort.SliceStable(total, func(i, j int) bool {
+            return total[i].tagua < total[j].tagua
+        })
+	    fmt.Printf("Cidade# %d:\n", count)
+        for i := 0;i < n-1;i++{
+			if total[i].tpess > 0 && total[i].tagua > 0{
+				fmt.Printf("%.0f-%.0f ", math.Floor((total[i].tpess*100) / 100), math.Floor((total[i].tagua*100) / 100))
+			}
+		}
+        fmt.Printf("%.0f-%.0f\n",  math.Floor((total[n-1].tpess*100) / 100), math.Floor((total[n-1].tagua*100) / 100))
+		fmt.Printf("Consumo medio: %.2f m3.\n", (math.Trunc(tAgua / tPess * 100)) / 100)
+    }
 
 }
-https://pkg.go.dev/sort#Float64s
