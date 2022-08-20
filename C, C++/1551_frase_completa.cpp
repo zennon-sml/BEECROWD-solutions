4bd50969380f0becb15da75985ce176cff7c385e
zennon-sml <zennonsampaioml@gmail.com>
2022-08-20 20:40:08 -0300

    1551 undone

---
+++ C, C++/1551_frase_completa.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int quantos(string frase){
+    int j;
+    vector alf = {1};
+    // for(j = 0;j < frase.length(); j++){
+    //     cout << frase[j] << '\n';
+    // }
+    return 0;
+}
+
+int main(){
+    int n, i, j;
+    cin >> n;
+    getchar();
+    for(i = 0;i < n;i++){
+        string frase;
+        getline(cin, frase);
+        quantos(frase);
+    }
+
+return 0;
+}
